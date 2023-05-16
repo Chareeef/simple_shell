5d5a3de5cbdc9c667827e6819fe56646d6aecbd3
A-fethi <fethiabderrahmane1@gmail.com>
2023-05-16 11:00:05 -0700

    HEADER FILE

--- main.h
+++ main.h
@@ -22,10 +22,32 @@ typedef struct PATH_DIR
 	struct PATH_DIR *next;
 } PATH_DIR;
 
+/**
+ * struct linked - ..
+ * @dir: ..
+ * @first: ..
+ * @next: ..
+ * @last: ..
+ */
+
+typedef	struct	linked
+{
+	char	*dir;
+	struct linked *next;
+	struct linked *first;
+	struct linked *last;
+} linked;
+
 int		execute_command(char *argv[]);
 void		free_all(char *line, char **tokens_list);
 int		build_path_list(PATH_DIR **head);
 PATH_DIR	*add_path(PATH_DIR **h, const char *str);
 char		*_getenv(const char *key);
+void		getdir(void);
+void		create_linkedlist(void);
+char		*_strdup(char *str);
+void		free_list(linked *h);
+int		_setenv(const char *name, const char *value, int overwrite);
+int		_unsetenv(const char *name);
 
 #endif
