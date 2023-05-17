a8c8b88591655cc8bccac7d4428024a3c3bb6a14
A-fethi <fethiabderrahmane1@gmail.com>
2023-05-17 11:42:27 -0700

    merg

--- search_exec.c
+++ search_exec.c
@@ -13,7 +13,7 @@ char	*search_exec(char *first_arg)
 	char	buffer[1024];
 	struct stat status;
 
-	env_path = _getenv("PATH");
+	env_path = strdup(_getenv("PATH"));
 	path_dir = strtok(env_path, ":");
 	while (path_dir != NULL)
 	{
@@ -25,9 +25,8 @@ char	*search_exec(char *first_arg)
 		strncpy(exec_file, buffer, strlen(buffer) + 1);
 		if (stat(exec_file, &status) == 0)
 			return (exec_file);
+		free(exec_file);
 		path_dir = strtok(NULL, ":");
 	}
-
-	free(exec_file);
 	return (NULL);
 }
