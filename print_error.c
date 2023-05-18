3bccac03957f3f6f5cd6ef8e8953ad610911a17a
A-fethi <fethiabderrahmane1@gmail.com>
2023-05-18 10:58:16 -0700

    0 in isatty hiya interactive 39el hhh

--- print_error.c
+++ print_error.c
@@ -6,7 +6,17 @@
   */
 void print_error(char *command)
 {
-	write(2, program_name, _strlen(program_name));
-	write(2, ": ", 2);
-	perror(command);
+	int	tty = 0;
+	if (isatty(tty))
+	{
+		write(2, program_name, _strlen(program_name));
+                write(2, ": ", 2);
+		perror(command);
+	}
+	else
+	{
+		write(2, program_name, _strlen(program_name));
+		write(2, ": 1: ", 5);
+                perror(command);
+	}
 }
