c1ad1179bc0eef63740f3f5bfde3151c113cb70a
Chareeef <youssef.charif.h@gmail.com>
2023-05-19 04:08:49 -0700

    count_error

--- print_error.c
+++ print_error.c
@@ -4,18 +4,12 @@
   * print_error - print error message
   * @command: first command line's argument
   */
-void print_error(char *command, int istty)
+void print_error(char *command)
 {
-	if (!istty)
-	{
-		write(2, program_name, _strlen(program_name));
-		write(2, ": 1: ", 5);
-                perror(command);
-	}
-	else
-	{
-		write(2, program_name, _strlen(program_name));
-                write(2, ": ", 2);
-		perror(command);
-	}
+	write(2, program_name, _strlen(program_name));
+	write(2, ": ", 2);
+	print_number(count_error);
+	write(2, ": ", 2);
+	perror(command);
+	count_error++;
 }
