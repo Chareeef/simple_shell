38be68b807746b9490946297e226d3438a00aefd
Chareeef <youssef.charif.h@gmail.com>
2023-05-19 04:38:54 -0700

    hotfix

--- execute_command.c
+++ execute_command.c
@@ -17,12 +17,12 @@ int execute_command(char *argv[])
 		return (-1);
 	}
 
-	if ((file_info.st_mode & S_IXUSR) != 0)
+	if ((file_info.st_mode & S_IXUSR) == 0)
 	{
 		print_error(argv[0]);
 		return (-1);
 	}
-	if (S_ISREG(file_info.st_mode) != 0)
+	if (S_ISREG(file_info.st_mode) == 0)
 	{
 		print_error(argv[0]);
 		return (-1);
