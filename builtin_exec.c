0e5ea67184957d2fc1d0a1f3c2f310e29f0d4974
A-fethi <fethiabderrahmane1@gmail.com>
2023-05-19 07:57:43 -0700

    exit status

--- builtin_exec.c
+++ builtin_exec.c
@@ -1,6 +1,53 @@
 #include "main.h"
 
 /**
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ *
+ * Return: A integer
+ */
+
+int    _atoi(char *s)
+{
+	int		i;
+	unsigned int	result;
+	int		sign;
+	int		j;
+
+	sign = 1;
+	i = 0;
+	result = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == '\n' || s[i] == '\v' || s[i] == '\t'
+				|| s[i] == '\f' || s[i] == '\r' || s[i] == ' ')
+			i++;
+
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[i] == '-' && s[i] == '+')
+		{
+			i++;
+		}
+		while ((s[i] >= '0' && s[i] <= '9'))
+		{
+			j = 1;
+			result = result * 10 + s[i] - '0';
+			i++;
+		}
+		if (s[i] == '\0' || j == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	return (result * sign);
+}
+
+/**
  * builtin_exec - look for builtin and execute it
  * @args: command line arguments
  * @line: command line
@@ -10,10 +57,21 @@
 
 int	builtin_exec(char **args, char *line)
 {
+	int	status;
+
 	if (_strcmp(args[0], "exit") == 0)
 	{
-		free_all(line, args);
-		exit(EXIT_SUCCESS);
+		if(args[1] != NULL)
+		{
+			status = _atoi(args[1]);
+			free_all(line, args);
+			exit(status);
+		}
+		else
+		{
+			free_all(line, args);
+			exit(EXIT_SUCCESS);
+		}
 	}
 	else if (_strcmp(args[0], "env") == 0)
 	{
