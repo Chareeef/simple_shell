f09e72daeeae592efefd702e3a675f1e52f50fe5
A-fethi <fethiabderrahmane1@gmail.com>
2023-05-19 11:18:54 -0700

    setenv/unsetenv

--- builtin_exec.c
+++ builtin_exec.c
@@ -28,7 +28,7 @@ int    _atoi(char *s)
 		{
 			sign *= -1;
 		}
-		if (s[i] == '-' && s[i] == '+')
+		if (s[i] == '+')
 		{
 			i++;
 		}
@@ -61,7 +61,7 @@ int	builtin_exec(char **args, char *line)
 
 	if (_strcmp(args[0], "exit") == 0)
 	{
-		if(args[1] != NULL)
+		if (args[1] != NULL)
 		{
 			status = _atoi(args[1]);
 			free_all(line, args);
@@ -76,6 +76,26 @@ int	builtin_exec(char **args, char *line)
 	else if (_strcmp(args[0], "env") == 0)
 	{
 		printenv();
+		return (1);
+	}
+	else if (_strcmp(args[0], "setenv") == 0)
+	{
+		setenv(args[1], args[2], 1);
+		if (!args[1] || !args[2])
+		{
+			write(2, "Usage: setenv VARIABLE VALUE\n", 29);
+			return (-1);
+		}
+		return (1);
+	}
+	else if (_strcmp(args[0], "unsetenv") == 0)
+	{
+		unsetenv(args[1]);
+		if (!args[1])
+		{
+			write(2, "Usage: unsetenv VARIABLE\n", 25);
+			return (-1);
+		}
 		return (1);
 	}
 	return (0);
